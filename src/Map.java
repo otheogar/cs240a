85d23590be9feb7bce40cc5227975339252b859f
Vaibhav Arora <vaibhavarora.nitt@gmail.com>
2013-03-13 00:29:39 -0700

    Formatting changes

--- src/Map.java
+++ src/Map.java
@@ -17,43 +17,41 @@ import java.util.Map.Entry;
 
 
 public class Map extends Mapper<LongWritable, Text, IntWritable, IntWritable > {
-		
-                private IntWritable id = new IntWritable();
-                private IntWritable adj_rating = new IntWritable();
+		private IntWritable id = new IntWritable();
+        private IntWritable adj_rating = new IntWritable();
 		
         @Override
 		public void map(LongWritable key, Text value, Context context) 
 			throws IOException, InterruptedException {
                                 
-                                String line = value.toString();
-                                
-                                //System.out.println(line);
-                                String[] keyval = line.split("\\t");
-                                System.out.println(keyval[0]);
-                                //uid.set(Integer.parseInt(entries[0]));//don't need the uid 
-                                String[] entries = keyval[1].split(";"); //items
-                                //create new hash to hold this user's ratings
-                                HashMap<Integer,Integer> items = new HashMap<Integer,Integer>();
-                                int sum = 0;
-                                for (String e : entries) {
-                                                String[] s = e.split(",");
-                                                int item_id = Integer.parseInt(s[0]);
-                                                int rating = Integer.parseInt(s[1]);
-                                                items.put(item_id,rating);
-                                                sum += rating;
-                                }
-                                int n = items.size();
-                                int avg = sum/n;
-                                IdAdjustedRating[] items_adj = new IdAdjustedRating[n];
-                                Iterator<Entry<Integer,Integer>> it = items.entrySet().iterator();
-                                
-                                while (it.hasNext()) {
-                                                Entry<Integer,Integer> en = it.next();
-                                                id.set(en.getKey());
-                                                adj_rating.set(en.getValue()/avg);
-                                                context.write(id,adj_rating);                
-                                }
-
+        	String line = value.toString();
+        	   //System.out.println(line);
+            String[] keyval = line.split("\\t");
+            System.out.println(keyval[0]);
+            //uid.set(Integer.parseInt(entries[0]));//don't need the uid 
+            String[] entries = keyval[1].split(";"); //items
+            //create new hash to hold this user's ratings
+            HashMap<Integer,Integer> items = new HashMap<Integer,Integer>();
+            int sum = 0;
+            for (String e : entries) {
+                            String[] s = e.split(",");
+                            int item_id = Integer.parseInt(s[0]);
+                            int rating = Integer.parseInt(s[1]);
+                            items.put(item_id,rating);
+                            sum += rating;
+            }
+            int n = items.size();
+            int avg = sum/n;
+            IdAdjustedRating[] items_adj = new IdAdjustedRating[n];
+            Iterator<Entry<Integer,Integer>> it = items.entrySet().iterator();
+            
+            while (it.hasNext()) {
+                            Entry<Integer,Integer> en = it.next();
+                            id.set(en.getKey());
+                            adj_rating.set(en.getValue()/avg);
+                            context.write(id,adj_rating);                
+            }
+                             
 			
 		}
 }
